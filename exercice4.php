44a66bbe3e41411db715f19263f734ddda275058
soufyane73 <82409191+soufyane73@users.noreply.github.com>
2025-09-30 15:15:00 +0100

    change in exercice4

--- exercice4.php
+++ exercice4.php
@@ -6,7 +6,24 @@ $cart = [
     ["name" => "Keyboard", "price" => 20, "quantity" => 2]
 ];
 
+function calculateTotal($cart) {
+    $totalItem = 0;
+    $total=0;
+    foreach ($cart as $item) {
+        $totalItem = $item["price"] * $item["quantity"];
+        if($item["quantity"] > 5) {
+            $totalItem *= 0.95; 
+        } 
+        $total += $totalItem;
+    }
 
+    if ($total > 100) {
+    return $total * 0.9;
+}
+    else {
+        return $total;
 
-
+}
+}
+echo calculateTotal($cart);
 ?>
